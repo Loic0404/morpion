d5d48eec6699dbf9ce65871bff254c05bd10e97f
Thomas-Dulompont <thomasdulompont.1@gmail.com>
2022-09-21 21:22:03 +0200

    Add Player 1 & Player 2

--- morpion.py
+++ morpion.py
@@ -1,20 +1,56 @@
 grille = {
-"A" : ["A1" , "A2", "A3"],
-"B" : ["B1" , "B2" , "B3"],
-"C" : ["C1" , "C2" , "C3"]
+"A" : ["" , "", ""],
+"B" : ["" , "" , ""],
+"C" : ["" , "" , ""]
 }
 
 tour = 0
 dernier_joueur = ""
+dernier_choix = ""
+symbole = "X"
 
-def jouer(var_dernier_joueur):
-    if var_dernier_joueur == "J1":
-        var_dernier_joueur = "J2"
-        print(var_dernier_joueur)
-        return var_dernier_joueur
+def est_vide(case):
+    if case == "":
+        case += symbole
+        return
+    else: 
+        print("Veuillez choisir une autre case ! ")
+
+while True:
+    if dernier_joueur == "J1":
+        dernier_joueur = "J2"
     else :
-        var_dernier_joueur = "J1"
-        print(var_dernier_joueur)
-        return var_dernier_joueur
+        dernier_joueur = "J1"
 
-jouer(dernier_joueur)
+    son_choix = input("Choisir une case : ")
+
+    if dernier_joueur != "J1":
+        symbole = "O"
+    else:
+        symbole = "X"
+
+    if len(son_choix[1]) > 2 and len(son_choix[1]) < 2:
+        print("Veuillez choisis une case correct ! (long)")
+    elif int(son_choix[1]) > 3:
+        print("Veuillez choisis une case correct ! (height)")
+    elif son_choix[0] == "A":
+        if grille["A"][int(son_choix[1]) -1] == "":
+            grille["A"][int(son_choix[1]) -1] += symbole
+        else:
+            print("Veuillez choisir une autre case ! ")
+    elif son_choix[0] == "B":
+        if grille["B"][int(son_choix[1]) -1] == "":
+            grille["B"][int(son_choix[1]) -1] += symbole
+        else:
+            print("Veuillez choisir une autre case ! ")
+    elif son_choix[0] == "C":
+        if grille["C"][int(son_choix[1]) -1] == "":
+            grille["C"][int(son_choix[1]) -1] += symbole
+        else:
+            print("Veuillez choisir une autre case ! ")
+    else:
+        print("Veuillez choisis une case correct ! (??)")
+
+    tour += 1
+
+    print(grille)
