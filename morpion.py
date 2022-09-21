4ff2bd01e2f12516ce651db99965a01ff7616413
Loic0404 <loic.clippe@outlook.fr>
2022-09-21 15:02:27 +0200

    update morpion

--- morpion.py
+++ morpion.py
@@ -1,2 +1,20 @@
-print("cocucou")
-print("test")
+grille = {
+"A" : ["A1" , "A2", "A3"],
+"B" : ["B1" , "B2" , "B3"],
+"C" : ["C1" , "C2" , "C3"]
+}
+
+tour = 0
+dernier_joueur = ""
+
+def jouer(var_dernier_joueur):
+    if var_dernier_joueur == "J1":
+        var_dernier_joueur = "J2"
+        print(var_dernier_joueur)
+        return var_dernier_joueur
+    else :
+        var_dernier_joueur = "J1"
+        print(var_dernier_joueur)
+        return var_dernier_joueur
+
+jouer(dernier_joueur)
