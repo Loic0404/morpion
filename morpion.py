c0d69aa576e5372f1c160b6d76ceee5253848ce1
Loic0404 <loic.clippe@outlook.fr>
2022-09-22 11:21:30 +0200

    modifaication du fichier

--- morpion.py
+++ morpion.py
@@ -9,6 +9,34 @@ dernier_joueur = ""
 dernier_choix = ""
 symbole = "X"
 
+def partie_gagner():
+    if grille["A"][0]== grille["A"][1] and grille["A"][0]==grille["A"][2]:
+        if len(grille["A"][0]) == 1 and len(grille["A"][1]) == 1 and len(grille["A"][2]) == 1:
+            return True
+    if grille["B"][0]== grille["B"][1] and grille["B"][0]==grille["B"][2]:
+        if len(grille["B"][0]) == 1 and len(grille["B"][1]) == 1 and len(grille["B"][2]) == 1:
+            return True
+    if grille["C"][0]== grille["C"][1] and grille["C"][0]==grille["C"][2]:
+        if len(grille["C"][0]) == 1 and len(grille["C"][1]) == 1 and len(grille["C"][2]) == 1:
+            return True
+    if grille["A"][0]== grille["B"][0] and grille["A"][0]==grille["C"][0]:
+        if len(grille["A"][0]) == 1 and len(grille["B"][0]) == 1 and len(grille["C"][0]) == 1:
+            return True
+    if grille["A"][1]== grille["B"][1] and grille["A"][1]==grille["C"][1]:
+        if len(grille["A"][1]) == 1 and len(grille["B"][1]) == 1 and len(grille["C"][1]) == 1:
+            return True
+    if grille["A"][2]== grille["B"][2] and grille["A"][2]==grille["C"][2]:
+        if len(grille["A"][2]) == 1 and len(grille["B"][2]) == 1 and len(grille["C"][2]) == 1:
+            return True
+    if grille["A"][0]== grille["B"][1] and grille["A"][0]==grille["C"][2]:
+        if len(grille["A"][0]) == 1 and len(grille["B"][1]) == 1 and len(grille["C"][2]) == 1:
+            return True
+    if grille["A"][2]== grille["B"][1] and grille["A"][2]==grille["C"][0]:
+        if len(grille["A"][2]) == 1 and len(grille["B"][1]) == 1 and len(grille["C"][0]) == 1:
+            return True
+
+
+
 while True:
 
     #AFFICHE GRILLE
@@ -34,19 +62,26 @@ while True:
     elif son_choix[0] == "A":
         if grille["A"][int(son_choix[1]) -1] == "":
             grille["A"][int(son_choix[1]) -1] += symbole
+            tour += 1
         else:
             print("Veuillez choisir une autre case ! ")
     elif son_choix[0] == "B":
         if grille["B"][int(son_choix[1]) -1] == "":
             grille["B"][int(son_choix[1]) -1] += symbole
+            tour += 1
         else:
             print("Veuillez choisir une autre case ! ")
     elif son_choix[0] == "C":
         if grille["C"][int(son_choix[1]) -1] == "":
             grille["C"][int(son_choix[1]) -1] += symbole
+            tour += 1
         else:
             print("Veuillez choisir une autre case ! ")
     else:
         print("Veuillez choisis une case correct ! (??)")
+    
+    if tour >= 5:
+        if partie_gagner():
+            print ("Partie Gagnée! ")
+            break
 
-    tour += 1
